f9b650a074d7744515bb620ef16a741c8f8d3d32
bobubuntu <musgravehill@gmail.com>
2023-09-26 18:03:20 +0300

    php attributes

--- patterns/index.php
+++ patterns/index.php
@@ -1,8 +1,46 @@
-<?php 
+<?php
 
-phpinfo();
-?>
+#[Attribute(Attribute::TARGET_CLASS)]
+class Listener
+{
+    public function __construct(
+        private ?string $title,
+        private ?string $desc
+    ) {
+    }
 
-See files *.php in root dir...
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+
+    /**
+     * @return self<>
+     */
+    public function getDesc()
+    {
+        return $this->desc;
+    }
+}
+
+#[Listener(title:'someTitle', desc: 'someDescription')]
+class somePublisher {
+
+}
+
+$rC = new ReflectionClass(somePublisher::class);
+$attrs = $rC->getAttributes(Listener::class);
+
+$listener = $attrs[0]->newInstance();
+echo $listener->getTitle() .'_'.$listener->getDesc();
+
+///////////////////// generic //////
+
+
+
+
+
+
 
 
